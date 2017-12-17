0e6811380d47895ae12e94e6c1c7f1f427bae1be
Iram Fattah <iramfattah94@gmail.com>
2017-12-16 23:19:39 -0500

    loaded best seller info without image

---
+++ AC-iOS-Unit4-Week1-HW/Models/BestSeller.swift
@@ -0,0 +1,68 @@
+//
+//  BestSeller.swift
+//  AC-iOS-Unit4-Week1-HW
+//
+//  Created by C4Q on 12/16/17.
+//  Copyright © 2017 C4Q . All rights reserved.
+//
+
+import Foundation
+struct BestSellerInfo: Codable {
+    let results: [BestSeller]
+}
+struct BestSeller: Codable {
+    let weeksOnList: Int
+    let bookDetails:[BookInfo]
+    enum CodingKeys: String, CodingKey {
+        case weeksOnList = "weeks_on_list"
+        case bookDetails = "book_details"
+    }
+    
+}
+struct BookInfo: Codable {
+    let title: String
+    let description: String
+    let primaryISBN13: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case primaryISBN13 = "primary_isbn13"
+    }
+}
+struct BestSellerAPIClient {
+    private init() {}
+    static let manager = BestSellerAPIClient()
+    func getBestSellers(with category: String, completionHandler: @escaping ([BestSeller]) -> Void, errorHandler: @escaping (Error) -> Void) {
+        let urlStr = "https://api.nytimes.com/svc/books/v3/lists.json?api-key=6c126b17173a46698e69341eac33049b&list=\(category)"
+        guard let url = URL(string: urlStr) else {
+            errorHandler(AppError.badURL(str: urlStr))
+            return
+        }
+        let request = URLRequest(url:url)
+        let parseDataInto: (Data) -> Void = {(data) in
+            do {
+                let results = try JSONDecoder().decode(BestSellerInfo.self, from: data)
+                
+            let bestSellers = results.results
+            completionHandler(bestSellers)
+            }
+            catch {
+                errorHandler(AppError.codingError(rawError: error))
+            }
+        }
+        NetworkHelper.manager.performDataTask(with: request, completionHandler: parseDataInto, errorHandler: errorHandler)
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
--- AC-iOS-Unit4-Week1-HW/Views/BestSellersViewController.swift
+++ AC-iOS-Unit4-Week1-HW/Views/BestSellersViewController.swift
@@ -15,10 +15,21 @@ class BestSellersViewController: UIViewController {
     
     @IBOutlet weak var categoryPickerView: UIPickerView!
     
-    
+    let cellSpacing = UIScreen.main.bounds.size.width * 0.05
     var categories = [Category]() {
         didSet {
             self.categoryPickerView.reloadAllComponents()
+        }
+    }
+    
+    var category = "" {
+        didSet {
+            loadBestSellersFromOnline()
+        }
+    }
+    var bestSellers = [BestSeller]() {
+        didSet {
+            self.bestSellerCollectionView.reloadData()
         }
     }
     
@@ -28,9 +39,12 @@ class BestSellersViewController: UIViewController {
         super.viewDidLoad()
         self.categoryPickerView.delegate = self
         self.categoryPickerView.dataSource = self
+        self.bestSellerCollectionView.dataSource = self 
         getCategoriesFromOnline()
    
     }
+    
+    
     func getCategoriesFromOnline() {
         let setCategories = {(onlineCategories: [Category]) in
             self.categories = onlineCategories
@@ -41,8 +55,32 @@ class BestSellersViewController: UIViewController {
         CategoryAPIClient.manager.getCategories(completionHandler: setCategories, errorHandler: printErrors)
 
     }
+    func loadBestSellersFromOnline() {
+        let setBestSellers = {(onlineBestSellers: [BestSeller]) in
+            self.bestSellers = onlineBestSellers
+        }
+        let printErrors = {(error: Error) in
+            print(error)
+        }
+        BestSellerAPIClient.manager.getBestSellers(with: category, completionHandler: setBestSellers, errorHandler: printErrors)
+    }
 
 }
+extension BestSellersViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return bestSellers.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = bestSellerCollectionView.dequeueReusableCell(withReuseIdentifier: "bestSellerCell", for: indexPath) as! BestSellerCollectionViewCell
+        let bestSeller = bestSellers[indexPath.row]
+        cell.weeksLabel.text = bestSeller.bookDetails[0].title
+        cell.summaryTextView.text = bestSeller.bookDetails[0].description
+        return cell
+    }
+    
+}
+
 extension BestSellersViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -55,5 +93,36 @@ extension BestSellersViewController: UIPickerViewDelegate, UIPickerViewDataSourc
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return categories[row].displayName
     }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.category = categories[row].encodedListName
+        
+    }
     
 }
+
+extension BestSellersViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numCells: CGFloat = 1
+        let numSpaces: CGFloat = numCells + 1
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let screenHeight = UIScreen.main.bounds.height
+        
+        return CGSize(width: (screenWidth - (cellSpacing * numSpaces)) / numCells, height: screenHeight * 0.25)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: cellSpacing, left: cellSpacing, bottom: 0, right: cellSpacing)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return cellSpacing
+    }
+}
+
+
+
